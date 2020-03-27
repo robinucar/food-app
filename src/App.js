d1dbe5a29de09324a6c276c52d14aaa6efa99c83
robinucar <mehmet.ucar@imediapro.co.uk>
2020-03-27 22:21:24 +0000

    set alert

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react'
 import './App.css'
 import Axios from 'axios'
 import Recipe from './Components/Recipe'
+import Alert from './Components/Alert'
 import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
-  const [query, setQuery] = useState('');
+  const [ query, setQuery ] = useState('');
 
-  const [recipes, setRecipes] = useState([]);
+  const [ recipes, setRecipes ] = useState([]);
+
+  const [ alert, setAlert ] = useState('');
 
   const APP_ID = '34acae91';
 
@@ -16,12 +19,20 @@ const App = () => {
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   const getData = async () => {
-    const result = await Axios.get(url)
-    setRecipes(result.data.hits)
+    if(query !== '') {
+      const result = await Axios.get(url)
+      if(!result.data.more){
+        return setAlert('No Meal with this name')
+      }
+      setRecipes(result.data.hits)
 
     console.log(result)
+    setAlert('');
+    setQuery('');
+    }else {
+      setAlert('Please Add a Meal Name!!!')
+    }
 
-    setQuery('')
   }
 
   const onChange = e =>{
@@ -38,6 +49,7 @@ const App = () => {
     <div className='App'>
       <h1>Food Searching App</h1>
       <form className = 'search-form' onSubmit = {onSubmit}>
+       {alert !== '' && <Alert alert = {alert}/>}
 
         <input
           type = 'text'
