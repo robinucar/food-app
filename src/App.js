1e4fd4bef2319e856acd98bc79658f9b09f74d26
robinucar <mehmet.ucar@imediapro.co.uk>
2020-03-27 16:54:58 +0000

    ui design, added api and states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import Axios from 'axios'
 
 const App = () => {
+  const [query, setQuery] = useState('');
+
+  const APP_ID = '34acae91';
+
+  const APP_KEY = '9ffe4f1ebfcd6589e1b1602da832fe4b';
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getData = async () => {
+    const result = await Axios.get(url)
+
+    console.log(result)
+
+    setQuery('')
+  }
+
+  const onChange = e =>{
+    setQuery(e.target.value)
+  }
+
+  const onSubmit = e => {
+    e.preventDefault();
+    getData()
+
+  }
+
   return (
     <div className='App'>
       <h1>Food Searching App</h1>
+      <form className = 'search-form' onSubmit = {onSubmit}>
+
+        <input
+          type = 'text'
+          placeholder = 'Search Food'
+          autoComplete = 'off'
+          onChange = {onChange}
+          value = {query}
+        />
+
+        <input type = 'submit' value = 'search' />
+
+      </form>
     </div>
   )
 }
